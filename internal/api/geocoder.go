07b6a98681eecb0c7a80e319f73ca88a72d26515
roxyash <roxyash@mail.ru>
2022-10-27 01:34:48 +0600

    some refactor

--- internal/api/geocoder.go
+++ internal/api/geocoder.go
@@ -11,18 +11,6 @@ import (
 type Coords struct {
 	Latitude  string `json:"geo_lat"`
 	Longitude string `json:"geo_lon"`
-	// Source: "москва сухонская 11",
-    // "result": "г Москва, ул Сухонская, д 11",
-    // "postal_code": "127642",
-    // "country": "Россия",
-    // "region": "Москва",
-    // "city_area": "Северо-восточный",
-    // "city_district": "Северное Медведково",
-    // "street": "Сухонская",
-    // "house": "11",
-    // "geo_lat": "55.8782557",
-    // "geo_lon": "37.65372",
-    // "qc_geo": 0
 }
 
 func Geocoder(address, token, secret string) ([]Coords, error) {
@@ -46,9 +34,6 @@ func Geocoder(address, token, secret string) ([]Coords, error) {
 	}
 
 	coords := []Coords{}
-	// var coords interface{}
-
-
 	err = json.Unmarshal(bodyText, &coords)
 	if err != nil {
 		return nil, err
--- internal/api/weather.go
+++ internal/api/weather.go
@@ -15,16 +15,13 @@ type WeatherInfo struct {
 
 type Main struct {
 	Temperature string `json:"temp"`
+
 }
-
-
-
-
 
 func GetWeather(lat, lon, appid string) (WeatherInfo, error) {
 	logrus.Info(lat, lon, appid)
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s", lat, lon, appid), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric", lat, lon, appid), nil)
 	logrus.Info(req.URL)
 
 	if err != nil {
@@ -39,15 +36,20 @@ func GetWeather(lat, lon, appid string) (WeatherInfo, error) {
 	if err != nil {
 		return WeatherInfo{}, nil
 	}
-
+	logrus.Info(string(bodyText))
 	logrus.Info(resp.StatusCode)
 
-	weatherInfo := WeatherInfo{}
+	type test struct {
+		Main map[string]string `json:"main"`
+	}
+	var weatherInfo map[string]interface{}
 
 	err = json.Unmarshal(bodyText, &weatherInfo)
 	if err != nil {
-		return WeatherInfo{}, nil
+		return WeatherInfo{}, err
 	}
-
-	return weatherInfo, nil
+	
+	logrus.Infof("TEST:%v", weatherInfo)
+	
+	return WeatherInfo{}, nil
 }
